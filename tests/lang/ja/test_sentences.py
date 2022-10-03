4f27734b3303f4c3190f676e1dfefffa1b3f7a47
sadahiroyoshi <sadahiroyoshi@gmail.com>
2022-10-03 19:07:36 +0900

    Revert "refactor"
    This reverts commit a060244f723025bfe2aeaa31ea6727ffcba9ed88.

--- tests/lang/ja/test_sentences.py
+++ tests/lang/ja/test_sentences.py
@@ -7,14 +7,6 @@ def reflector(nlp_ja):
     return Reflector(nlp_ja)
 
 
-@pytest.mark.parametrize(
-    "message",
-    [
-        "",
-        " ",  # 半角スペース
-        "　",  # 全角スペース
-    ],
-)
-def test_no_sentence(reflector, message):
-    response = reflector.reflect(message)
+def test_no_sentence(reflector):
+    response = reflector.reflect("")
     assert response is None
