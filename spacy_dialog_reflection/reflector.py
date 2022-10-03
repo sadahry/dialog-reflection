58084bfdc3309babb5ac4d9fb5774d8bcf288979
sadahiroyoshi <sadahiroyoshi@gmail.com>
2022-10-03 19:02:48 +0900

    Revert "add vscode settings"
    This reverts commit 709872d2caa7332555e10413c29991c84789d0a6.

--- spacy_dialog_reflection/reflector.py
+++ spacy_dialog_reflection/reflector.py
@@ -1,28 +1,22 @@
 from typing import Optional
 import spacy
 
-
 class Reflector:
     def __init__(self, nlp: spacy.Language):
         self.nlp = nlp
-        self.buidler = ReflectionBuilder()
 
     @classmethod
     def from_name(cls, name: str):
         return cls(spacy.load(name))
 
-    def reflect(self, message: str) -> Optional[str]:
+    def reflect(self, message: str)-> Optional[str]:
         doc = self.nlp(message)
-        return self.buidler.build(doc)
+        return self.reflect_from_doc(doc)
 
-
-class ReflectionBuilder:
-    def build(self, doc: spacy.tokens.Doc) -> Optional[str]:
+    @classmethod
+    def reflect_from_doc(self, doc: spacy.tokens.Doc)-> Optional[str]:
         if doc.text == "":
             return None
-        sent = self._select_sentence(doc)
+        sent = next(doc.sents)
 
         return sent.text
-
-    def _select_sentence(self, doc: spacy.tokens.Doc) -> spacy.tokens.Span:
-        return next(doc.sents)
