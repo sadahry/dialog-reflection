7a52abf780d39a80ba6638f0d3256aa4e091295c
sadahiroyoshi <sadahiroyoshi@gmail.com>
2022-10-03 18:43:41 +0900

    add test

---
+++ tests/conftest.py
@@ -0,0 +1,6 @@
+import pytest
+import spacy
+
+@pytest.fixture(scope="session")
+def nlp_ja():
+    return spacy.load('ja_ginza')
---
+++ tests/lang/ja/test_sentences.py
@@ -0,0 +1,10 @@
+import pytest
+from spacy_dialog_reflection.reflector import Reflector
+
+@pytest.fixture(scope="session")
+def reflector(nlp_ja):
+    return Reflector(nlp_ja)
+
+def test_no_sentence(reflector):
+    response = reflector.reflect("")
+    assert response is None
