aa57c669bc1b155dea46ba50c2fc93a212c4dcd5
sadahiroyoshi <sadahiroyoshi@gmail.com>
2022-10-03 19:06:01 +0900

    refactor

--- spacy_dialog_reflection/reflector.py
+++ spacy_dialog_reflection/reflector.py
@@ -1,22 +1,28 @@
 from typing import Optional
 import spacy
 
+
 class Reflector:
     def __init__(self, nlp: spacy.Language):
         self.nlp = nlp
+        self.buidler = ReflectionBuilder()
 
     @classmethod
     def from_name(cls, name: str):
         return cls(spacy.load(name))
 
-    def reflect(self, message: str)-> Optional[str]:
+    def reflect(self, message: str) -> Optional[str]:
         doc = self.nlp(message)
-        return self.reflect_from_doc(doc)
+        return self.buidler.build(doc)
 
-    @classmethod
-    def reflect_from_doc(self, doc: spacy.tokens.Doc)-> Optional[str]:
+
+class ReflectionBuilder:
+    def build(self, doc: spacy.tokens.Doc) -> Optional[str]:
         if doc.text == "":
             return None
-        sent = next(doc.sents)
+        sent = self._select_sentence(doc)
 
         return sent.text
+
+    def _select_sentence(self, doc: spacy.tokens.Doc) -> spacy.tokens.Span:
+        return next(doc.sents)
