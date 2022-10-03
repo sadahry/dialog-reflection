8ed3b0447b652964de150938bcaf56422bd3ea5a
sadahiroyoshi <sadahiroyoshi@gmail.com>
2022-10-03 19:46:48 +0900

    add _select_sentence

--- spacy_dialog_reflection/reflector.py
+++ spacy_dialog_reflection/reflector.py
@@ -5,7 +5,7 @@ import spacy
 class Reflector:
     def __init__(self, nlp: spacy.Language):
         self.nlp = nlp
-        self.buidler = ReflectionBuilder()
+        self.builder = ReflectionBuilder()
 
     @classmethod
     def from_name(cls, name: str):
@@ -13,16 +13,30 @@ class Reflector:
 
     def reflect(self, message: str) -> Optional[str]:
         doc = self.nlp(message)
-        return self.buidler.build(doc)
+        return self.builder.build(doc)
 
 
 class ReflectionBuilder:
-    def build(self, doc: spacy.tokens.Doc) -> Optional[str]:
+    ROOT_POS_SET = {"VERB", "NOUN", "PROPN", "ADJ"}
+
+    def build(
+        self,
+        doc: spacy.tokens.Doc,
+    ) -> Optional[str]:
         if doc.text.strip() == "":
+            print("empty message")
             return None
         sent = self._select_sentence(doc)
 
         return sent.text
 
-    def _select_sentence(self, doc: spacy.tokens.Doc) -> spacy.tokens.Span:
-        return next(doc.sents)
+    def _select_sentence(
+        self,
+        doc: spacy.tokens.Doc,
+    ) -> Optional[spacy.tokens.Span]:
+        for sent in reversed(list(doc.sents)):
+            if sent.root.pos_ in self.ROOT_POS_SET:
+                return sent
+
+        print("no valid sentenses")
+        return None
--- tests/lang/ja/test_sentences.py
+++ tests/lang/ja/test_sentences.py
@@ -18,3 +18,48 @@ def reflector(nlp_ja):
 def test_no_sentence(reflector, message, assert_message):
     response = reflector.reflect(message)
     assert response is None, assert_message
+
+
+class TestReflectionBuilder:
+    """Test of ReflectionBuilder private methods"""
+
+    @pytest.mark.parametrize(
+        "message, expected, assert_message",
+        [
+            (
+                "今日は旅行に行きました",
+                "今日は旅行に行きました",
+                "extract sentence",
+            ),
+            (
+                "こんにちは。今日は旅行に行きました",
+                "今日は旅行に行きました",
+                "extract last sentence VERB",
+            ),
+            (
+                "こんにちは。今日は旅行に行きました。最高でした",
+                "最高でした",
+                "extract last sentence NOUN",
+            ),
+            (
+                "こんにちは。今日は旅行に行きました。田中さんとです",
+                "田中さんとです",
+                "extract last sentence PROUN",
+            ),
+            (
+                "こんにちは。今日は旅行に行きました。楽しかったです",
+                "楽しかったです",
+                "extract last sentence ADJ",
+            ),
+        ],
+    )
+    def test_select_sentence(
+        self,
+        reflector: Reflector,
+        message,
+        expected,
+        assert_message,
+    ):
+        doc = reflector.nlp(message)
+        response = reflector.builder._select_sentence(doc)
+        assert response.text == expected, assert_message
